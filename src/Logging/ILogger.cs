1b51156bd35cc62af3e6c8e89c3e3ed09283ab0b
Alberto De Pena <depenaj@rushenterprises.com>
2019-12-18 15:12:31 -0600

    Added logging project

---
+++ src/Logging/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Numaka.Common.Logging
+{
+    public interface ILogger : IDisposable
+    {
+        void LogException(string message, Exception exception);
+
+        void LogMessage(string message);
+
+        void LogWarning(string message);
+    }
+}
---
+++ src/Logging/Logger.cs
@@ -0,0 +1,55 @@
+﻿using Serilog;
+using System;
+using System.IO;
+
+namespace Numaka.Common.Logging
+{
+    public class Logger : ILogger
+    {
+        private readonly Serilog.Core.Logger _logger;
+        private bool _disposed;
+
+        public Logger(string serviceName, bool isDebugMode = false)
+        {
+            if (string.IsNullOrWhiteSpace(serviceName)) throw new ArgumentNullException(nameof(serviceName));
+
+            var filePath = $"{Directory.GetCurrentDirectory()}/Logs/{serviceName}.log";
+
+            _logger =
+                (isDebugMode ? new LoggerConfiguration().MinimumLevel.Debug() : new LoggerConfiguration().MinimumLevel.Warning())
+                .WriteTo.Console()
+                .WriteTo.File(filePath, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
+                .CreateLogger();
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <inheritdoc />
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+
+            if (disposing)
+            {
+                _logger.Information("Disposing logger...");
+                _logger.Dispose();
+            }
+
+            _disposed = true;
+        }
+
+        /// <inheritdoc />
+        public void LogException(string message, Exception exception) => _logger.Error(exception, message);
+
+        /// <inheritdoc />
+        public void LogMessage(string message) => _logger.Information(message);
+
+        /// <inheritdoc />
+        public void LogWarning(string message) => _logger.Warning(message);
+    }
+}
